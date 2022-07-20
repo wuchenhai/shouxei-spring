7aa9b0308019d485fe507083f7de56a8cba2a51e
wuchenhai <123>
2022-07-20 15:55:16 +0800

    手写spring

--- src/main/java/com/luban/service/HaiBeanPostProcessor.java
+++ src/main/java/com/luban/service/HaiBeanPostProcessor.java
@@ -4,8 +4,7 @@ import com.spring.BeanPostProcessor;
 import com.spring.Component;
 
 @Component
-public class LubanBeanPostProcessor implements BeanPostProcessor {
-
+public class HaiBeanPostProcessor implements BeanPostProcessor {
     @Override
     public void postProcessBeforeInitialization(String beanName, Object bean) {
 
@@ -13,8 +12,6 @@ public class LubanBeanPostProcessor implements BeanPostProcessor {
 
     @Override
     public void postProcessAfterInitialization(String beanName, Object bean) {
-        // 在bean实例化完毕后，在显示调用初始化方法的前后添加我们自己的逻辑
-        // aop在这里创建一个代理对象，并且把原始对象传入到代理对象的构造函数中
         System.out.println("postProcessAfterInitialization");
     }
 }
